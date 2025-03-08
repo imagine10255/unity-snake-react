f196db29d4413d4b8a66667db4ce4ee31cbd6f0e
imagine10255 <imagine10255@gmail.com>
2025-03-09 00:12:07 +0800

    新增 傳遞訊息給 unity

--- src/views/Example/Example.tsx
+++ src/views/Example/Example.tsx
@@ -1,17 +1,22 @@
 import {useEffect, useRef, useState} from 'react';
 import { Unity, useUnityContext } from "react-unity-webgl";
+import Button from "../../components/Button";
 
 
 
 const Example = () => {
     const [isVisible, setVisible] = useState(true);
-    const { unityProvider } = useUnityContext({
+    const { unityProvider, sendMessage } = useUnityContext({
         loaderUrl: "/data/Snake.loader.js",
         dataUrl: "/data/Snake.data.br",
         frameworkUrl: "/data/Snake.framework.js.br",
         codeUrl: "/data/Snake.wasm.br",
     });
 
+
+    const send = () => {
+        sendMessage("snake_0", "SpawnEnemies", 999); // 调用send函数
+    }
 
     const renderContent = () => {
         return <Unity unityProvider={unityProvider} style={{width: '100%', height: '100%'}}/>;
@@ -21,6 +26,8 @@ const Example = () => {
     return <div style={{width: '100%', height: '100%', margin: '0 auto'}}>
 
         {renderContent()}
+
+        <Button onClick={send}>Set PrevScroe</Button>
     </div>;
 };
 
